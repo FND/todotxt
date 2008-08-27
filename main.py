0e409914134af36d313b6d1971aa5c23b339d5f2
FND <fnd@fnd-osmosoft.(none)>
2008-08-27 20:56:06 +0100

    added basic Items class

--- main.py
+++ main.py
@@ -16,6 +16,43 @@ import sys
 def main(args = []):
 	return # TODO
 
+class Items:
+	def __init__(self):
+		self.active = []
+		self.closed = []
+
+	def get(self, source): # XXX: does not belong here?
+		pass # TODO
+
+	def add(self, text):
+		"""
+		add new active item
+
+		@param text: item text
+		@type  text: str
+		@return: item index
+		@rtype : int
+		"""
+		self.active.append(text)
+		return len(self.active) - 1
+
+	def remove(self, id):
+		"""
+		remove active item
+
+		@param id: item ID
+		@type  id: int
+		@return: item text
+		@rtype : str
+		"""
+		return self.active.pop(id)
+
+	def modify(self, id, text):
+		pass # TODO
+
+	def archive(self, id):
+		pass # TODO
+
 if __name__ == "__main__":
 	sys.exit(main(sys.argv))
 
--- test_main.py
+++ test_main.py
@@ -13,6 +13,48 @@ class mainTestCase(unittest.TestCase):
 		expected = None
 		self.assertEquals(expected, main.main())
 
+class ItemsTestCase(unittest.TestCase):
+	def setUp(self):
+		self.items = main.Items()
+		pass
+
+	def tearDown(self):
+		pass
+
+	def testInitCreatesActiveItems(self):
+		"""__init__ creates active items"""
+		expected = []
+		self.assertEquals(expected, self.items.active)
+
+	def testInitCreatesClosedItems(self):
+		"""__init__ creates closed items"""
+		expected = []
+		self.assertEquals(expected, self.items.closed)
+
+	def testAddAppendsItem(self):
+		"""add creates new active item"""
+		self.items.add("foo")
+		expected = ["foo"]
+		self.assertEquals(expected, self.items.active)
+
+	def testAddReturnsID(self):
+		"""add returns new item ID"""
+		expected = 0
+		self.assertEquals(expected, self.items.add("foo"))
+
+	def testRemoveRemovesItem(self):
+		"""remove deletes specified active item"""
+		self.items.active = ["foo", "bar", "baz"]
+		self.items.remove(1)
+		expected = ["foo", "baz"]
+		self.assertEquals(expected, self.items.active)
+
+	def testRemoveReturnsText(self):
+		"""remove returns deleted item's text"""
+		self.items.active = ["foo", "bar", "baz"]
+		expected = "bar"
+		self.assertEquals(expected, self.items.remove(1))
+
 if __name__ == "__main__":
 	unittest.main()
 
