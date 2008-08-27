000d38d26df00d18bd11a6fd20504467b14bad44
FND <fnd@fnd-osmosoft.(none)>
2008-08-27 17:39:32 +0100

    established basic framework

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+TODO.TXT Manager
+
+Author: FND (http://fnd.lewcid.org/blog/)
+License: GPL (http://www.gnu.org/copyleft/gpl.html)
+Version: 2.0.0 alpha
+
+Based on concept by Gina Trapani (http://todotxt.com).
+Original Python port by Shane Koster.
+"""
+
+import sys
+
+def main(args = []):
+	return # TODO
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv))
+
---
+++ test_main.py
@@ -0,0 +1,18 @@
+import unittest
+import main
+
+class mainTestCase(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def testMainReturnsNoneOnSuccess(self):
+		"""main returns None on success"""
+		expected = None
+		self.assertEquals(expected, main.main())
+
+if __name__ == "__main__":
+	unittest.main()
+
