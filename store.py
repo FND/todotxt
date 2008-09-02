6aaf6d20190e9b829568b5a63ae3f9147c07377f
FND <fnd@fnd-osmosoft.(none)>
2008-09-02 13:45:57 +0100

    added store module (FileStore class)

---
+++ store.py
@@ -0,0 +1,42 @@
+class FileStore:
+	def __init__(self, active, closed, report):
+		"""
+		@param active (str): filepath for active items
+		@param closed (str): filepath for closed items
+		@param report (str): filepath for report
+		@return: None
+		"""
+		self.active = active
+		self.closed = closed
+		self.report = report
+
+	def get(self, entity):
+		"""
+		retrieve data from file
+
+		@param entity (str): storage entity (active, closed, report)
+		@return: None
+		@raise IOError: cannot read file
+		"""
+		filepath = getattr(self, entity)
+		return [i.rstrip() for i in open(filepath, "r")]
+
+	def put(self, entity, items, append):
+		"""
+		write data to file
+
+		@param entity (str): storage entity (active, closed, report)
+		@param items (list): items
+		@param append (bool): use append rather than overwriting existing data
+		@return: None
+		@raise IOError: cannot write to file
+		"""
+		filepath = getattr(self, entity)
+		if append:
+			f = open(filepath, "a")
+			f.write("\n%s" % "\n".join(items))
+		else:
+			f = open(filepath, "w")
+			f.write("\n".join(items))
+		f.close()
+
---
+++ test_store.py
@@ -0,0 +1,52 @@
+import unittest
+import os
+import shutil
+import store
+
+class FileStoreTestCase(unittest.TestCase):
+	def setUp(self):
+		self.files = ["/tmp/foo", "/tmp/bar", "/tmp/baz"]
+		self.store = store.FileStore(*self.files)
+		try:
+			shutil.rmtree("/tmp/test/")
+		except OSError:
+			pass
+		os.mkdir("/tmp/test")
+		for filename in self.files:
+			f = open(filename, "w")
+			f.write("lorem ipsum\ndolor sit amet")
+			f.close()
+
+	def tearDown(self):
+		shutil.rmtree("/tmp/test/")
+
+	def testInitRequiresArguments(self):
+		"""__init__ requires arguments for active, closed and report"""
+		expected = TypeError
+		self.assertRaises(expected, store.FileStore)
+		self.assertRaises(expected, store.FileStore, "foo")
+		self.assertRaises(expected, store.FileStore, "foo", "bar")
+		store.FileStore("foo", "bar", "baz")
+
+	def testGetReturnsFileContents(self):
+		"""get returns file contents as list of lines"""
+		expected = ["lorem ipsum", "dolor sit amet"]
+		self.assertEqual(expected, self.store.get("active"))
+
+	def testPutCreatesFile(self):
+		"""put creates (or overrides) file"""
+		self.store.put("active", ["foo bar", "baz"], False)
+		contents = open(self.store.active).read()
+		expected = "foo bar\nbaz"
+		self.assertEqual(expected, contents)
+
+	def testPutAppendsFile(self):
+		"""put appends file contents"""
+		self.store.put("active", ["foo bar", "baz"], True)
+		contents = open(self.store.active).read()
+		expected = "lorem ipsum\ndolor sit amet\nfoo bar\nbaz"
+		self.assertEqual(expected, contents)
+
+if __name__ == "__main__":
+	unittest.main()
+
